1b0a53646799ddda69ff313d96e9541fad50cfe4
sourabh <sourabh.debnath@goodworklabs.com>
2017-10-18 17:46:24 +0530

    Added- Error checker

--- templates/create.js
+++ templates/create.js
@@ -3,11 +3,11 @@ const getTitleCase = require('../utilities/utility').getTitleCase;
 const setLabels = require('../utilities/utility').setLabels;
 
 let localeFields = {};
-
+let errors = {};
 let getFieldsFromInnerObject = function(objectName, fields, definition, module, jPath, isArray) {
     if (definition[objectName])
         for (let key in definition[objectName].properties) {
-            if (["id","tenantId", "auditDetails", "assigner"].indexOf(key) > -1) continue;
+            if (["id", "tenantId", "auditDetails", "assigner"].indexOf(key) > -1) continue;
 
             if (definition[objectName].properties[key].type == "array") {
                 let refSplitArr = definition[objectName].properties[key].items.$ref.split("/");
@@ -45,8 +45,8 @@ let createTemplate = function(module, numCols, path, config, definition, uiInfoD
     let fields = {};
     let isArr = false;
     let ind = 0;
-    for(var i=0; i<config["post"].parameters.length; i++) {
-        if(config["post"].parameters[i].schema) {
+    for (var i = 0; i < config["post"].parameters.length; i++) {
+        if (config["post"].parameters[i].schema) {
             ind = i;
             break;
         }
@@ -56,7 +56,7 @@ let createTemplate = function(module, numCols, path, config, definition, uiInfoD
     for (let key in properties) {
         if (key != "requestInfo") {
             //IF ARRAY
-            if(properties[key].type == "array") {
+            if (properties[key].type == "array") {
                 let propertiesArr = properties[key].items.$ref.split("/");
                 specifications.objectName = propertiesArr[propertiesArr.length - 1];
                 isArr = true;
@@ -69,43 +69,56 @@ let createTemplate = function(module, numCols, path, config, definition, uiInfoD
     }
 
     getFieldsFromInnerObject(specifications.objectName, fields, definition, module, isArr ? (specifications.objectName + "[0]") : specifications.objectName);
-    
+
     //=======================CUSTOM FILE LOGIC==========================>>
-    if(uiInfoDef.ExternalData && typeof uiInfoDef.ExternalData == "object" && Object.keys(uiInfoDef.ExternalData).length) {
-    	for(var key in uiInfoDef.ExternalData) {
-    		if(fields[key]) fields[key].url = uiInfoDef.ExternalData[key];
-    	}
+    if (uiInfoDef.ExternalData && typeof uiInfoDef.ExternalData == "object" && Object.keys(uiInfoDef.ExternalData).length) {
+        for (var key in uiInfoDef.ExternalData) {
+            if (fields[key]) fields[key].url = uiInfoDef.ExternalData[key];
+            else
+                errors[key] = "Field exists in x-ui-info ExternalData section but not present in API specifications.";
+        }
     }
 
-    if(uiInfoDef.dependents && uiInfoDef.dependents.length) {
-    	for(let i=0; i<uiInfoDef.dependents.length; i++) {
-    		if(fields[uiInfoDef.dependents[i].onChangeField]) {
-    			fields[uiInfoDef.dependents[i].onChangeField].depedants = [];
-    			for(let key in uiInfoDef.dependents[i].affectedFields) {
-    				fields[uiInfoDef.dependents[i].onChangeField].depedants.push({
-    					"jsonPath": key,
-    					"type": uiInfoDef.dependents[i].affectedFields[key].type,
-    					"pattern": uiInfoDef.dependents[i].affectedFields[key].pattern
-    				})
-    			}
-    		}
-    	}
+    if (uiInfoDef.dependents && uiInfoDef.dependents.length) {
+        for (let i = 0; i < uiInfoDef.dependents.length; i++) {
+            if (fields[uiInfoDef.dependents[i].onChangeField]) {
+                fields[uiInfoDef.dependents[i].onChangeField].depedants = [];
+                for (let key in uiInfoDef.dependents[i].affectedFields) {
+                    fields[uiInfoDef.dependents[i].onChangeField].depedants.push({
+                        "jsonPath": key,
+                        "type": uiInfoDef.dependents[i].affectedFields[key].type,
+                        "pattern": uiInfoDef.dependents[i].affectedFields[key].pattern
+                    })
+                }
+            }
+        }
     }
 
-    for(var key in uiInfoDef.groups) {
+    for (var key in uiInfoDef.groups) {
         localeFields[module + ".create.group.title." + key] = getTitleCase(key);
-    	let group = {
-    		name: key,
-    		label: module + ".create.group.title." + key,
-    		fields: []
-    	};
-    	for(var i=0; i<uiInfoDef.groups[key].fields.length; i++) {
-    		group.fields.push(fields[uiInfoDef.groups[key].fields[i]]);
-    	}
-    	specifications.groups.push(group);
+        let group = {
+            name: key,
+            label: module + ".create.group.title." + key,
+            fields: []
+        };
+        for (var i = 0; i < uiInfoDef.groups[key].fields.length; i++) {
+            if (fields[uiInfoDef.groups[key].fields[i]])
+                group.fields.push(fields[uiInfoDef.groups[key].fields[i]]);
+            else
+                errors[fields[uiInfoDef.groups[key].fields[i]]] = "Field exists in x-ui-info groups section but not present in API specifications.";
+        }
+        specifications.groups.push(group);
     }
 
     setLabels(localeFields);
+
+    if(Object.keys(errors).length) {
+        console.log("\x1b[31mERROR OCCURED! CANNOT PROCESS YAML.");
+        console.log(JSON.stringify(errors));
+        console.log("\x1b[37m");
+        process.exit();
+    }
+
     //==================================================================>>
     return specifications;
 }
